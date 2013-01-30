dca5fc908fc891fe7fb7e069dc3f99bc3e215796
marcin- <marcin.bojara@gmail.com>
2013-01-30 14:02:13 +0100

    ntfs-3g: version bump, merged ntfsprogs

--- system/base/ntfs-3g/actions.py
+++ system/base/ntfs-3g/actions.py
@@ -14,43 +14,21 @@ from pisi.actionsapi import get
 def setup():
     shelltools.export("CFLAGS", "%s -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
     autotools.configure("--disable-static \
-                         --disable-ldconfig \
-                         --sbindir=/sbin \
-                         --bindir=/bin \
-                         --libdir=/usr/lib \
-                         --docdir=/usr/share/doc/%s" % get.srcNAME())
+                         --with-fuse=external \
+                         --enable-extras \
+                         --enable-posix-acls \
+                         --enable-ldscript \
+                         --disable-ldconfig")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.insinto("/usr/lib/pkgconfig", "libntfs-3g/*.pc")
+    pisitools.domove("/usr/bin/ntfs-3g.*", "/bin")
 
     # Create some compat symlinks
+    pisitools.dosym("/bin/ntfs-3g", "/sbin/mount.ntfs")
 
-    pisitools.dosym("mount.ntfs-3g", "/sbin/mount.ntfs-fuse")
-    pisitools.dosym("mount.ntfs-3g", "/sbin/mount.ntfs")
-    pisitools.dosym("ntfs-3g", "/bin/ntfsmount")
-
-    pisitools.dosym("/bin/ntfs-3g", "/usr/bin/ntfs-3g")
-    pisitools.dosym("/bin/ntfsmount", "/usr/bin/ntfsmount")
-
-    pisitools.remove("/usr/share/man/man8/ntfsfix.8")
-    pisitools.remove("/usr/share/man/man8/ntfsclone.8")
-    pisitools.remove("/usr/share/man/man8/ntfscmp.8")
-    pisitools.remove("/usr/share/man/man8/ntfsresize.8")
-    pisitools.remove("/sbin/mkfs.ntfs")
-    pisitools.remove("/usr/share/man/man8/ntfscluster.8")
-    pisitools.remove("/usr/share/man/man8/ntfsinfo.8")
-    pisitools.remove("/usr/share/man/man8/ntfslabel.8")
-    pisitools.remove("/usr/share/man/man8/mkfs.ntfs.8")
-    pisitools.remove("/usr/share/man/man8/ntfsundelete.8")
-    pisitools.remove("/usr/share/man/man8/ntfscp.8")
-    pisitools.remove("/usr/share/man/man8/ntfsls.8")
-    pisitools.remove("/usr/share/man/man8/mkntfs.8")
-    pisitools.remove("/usr/share/man/man8/ntfsprogs.8")
-    pisitools.remove("/usr/share/man/man8/ntfscat.8")
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "CREDITS", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "CREDITS", "NEWS", "README")
+    pisitools.dosym("/usr/share/doc/ntfs-3g", "/usr/share/doc/ntfsprogs")
