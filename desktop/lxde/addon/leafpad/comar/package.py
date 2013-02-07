4d8a24501a920a9c0d753cea22218eef40546920
circass <erdincgultekin@gmail.com>
2013-02-07 21:54:33 +0200

    Revert "leafpad comar script"
    This reverts commit f2a7023294fe51b6fa56a38f1c3245f96b9e262a.

--- desktop/lxde/addon/leafpad/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
- 
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
- 
-def postRemove(fromVersion, fromRelease, toVersion, toRelease):
-     os.system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
