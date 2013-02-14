0aa457668dc8dccb0e77ff3a32fcab619215a8ca
Ertan Güven <guven.ertan@gmail.com>
2013-02-14 23:24:41 +0200

    Pardus ANKA name must be specified.

--- util/archive/par2cmd/actions.py
+++ util/archive/par2cmd/actions.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyleft 2012 Pardus ANKA Community
+# Copyright 2005-2011 TUBITAK/UEAKE
 # Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 
