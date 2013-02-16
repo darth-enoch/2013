4237fe6b101e1b69b8b27a498ea5a8660d0c1ac5
obsoleteman <tulliana@gmail.com>
2013-02-16 22:19:21 +0200

    qsuashfs-tools: Change compression type to xz, and some other enhancements

--- hardware/disk/squashfs-tools/actions.py
+++ hardware/disk/squashfs-tools/actions.py
@@ -11,10 +11,18 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-#WorkDir = "%s_%s" % (get.srcNAME(), get.srcVERSION())
-
 def build():
     shelltools.cd("squashfs-tools")
+    #Chakra features
+    #reduce memory requirements of unsquashfs to support installation on systems with 256 MB RAM 
+    cmd1='sed -i -e "s/BUFFER_DEFAULT [0-9]*/BUFFER_DEFAULT 32/" unsquashfs.h'
+    cmd2="sed -i 's|^#XZ_SUPPORT = 1|XZ_SUPPORT = 1|' Makefile"
+    cmd3="sed -i 's|^#LZO_SUPPORT = 1|LZO_SUPPORT = 1|' Makefile"
+    cmd4="sed -i 's|^COMP_DEFAULT = gzip|COMP_DEFAULT = xz|' Makefile"
+    cmds=[cmd1,cmd2,cmd3,cmd4]
+        
+    for cmd in cmds:
+        shelltools.system(cmd)
     autotools.make('RPM_OPT_FLAGS="%s"' % get.CFLAGS())
 
 def install():
